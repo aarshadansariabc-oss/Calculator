2e596069f20ecc5de54baffe4efcb2a76973ffc8
Arshad Ansari <aarshadansariabc@gmail.com>
2025-08-27 17:54:40 +0530

    lol

--- sw.js
+++ sw.js
@@ -2,19 +2,21 @@ const CACHE_NAME = "calculator-cache-v1";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js",
+  "./style.css",
+  "./script.js",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
+// Install SW and cache files
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
+// Fetch cached files
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
